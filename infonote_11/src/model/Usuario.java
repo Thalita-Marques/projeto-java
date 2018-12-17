c663c5ce8060c18525d3dcda644dae3180946c48
Thalita Marques <thata.marques1561@gmail.com>
2018-12-17 20:35:48 -0200

    infonote_11

---
+++ infonote_11/src/model/Usuario.java
@@ -0,0 +1,57 @@
+package model;
+
+public abstract class Usuario {
+	
+	private String login;
+	private String senha;
+	private int tipo;
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public Usuario() {
+		super();
+	}
+	
+	public Usuario(String login, String senha, int tipo) {
+		this.login = login;
+		this.senha = senha;
+		this.tipo = tipo;	
+	}
+	
+	@Override
+	public String toString(){
+	final String ENTER = "\n";
+	String retValue = "";
+	
+	retValue = "Informações sobre o Usuario:" + ENTER +
+	super.toString()+
+	"Login:: " + login + ENTER +
+	"Senha: " + senha + ENTER +
+	"tipo: " + tipo + ENTER;
+	
+	return retValue;
+	}
+	
+}
